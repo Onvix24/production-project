18eeec098d8809f934626eda484ba84d4881e039
Onvix24 <maksdark2004@gmail.com>
2023-10-16 23:55:40 +0300

    add jest, add first test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
 	env: {
 		"browser": true,
-		"es2021": true
+		"es2021": true,
+		"jest": true,
 	},
 	extends: [
 		"eslint:recommended",
@@ -37,7 +38,8 @@ module.exports = {
 		"import/extension": "off",
 		"@typescript-eslint/ban-ts-comment": "warn",
 		"no-underscore-dangle": "off",
-		"i18next/no-literal-string": ["error", { markupOnly: true }]
+		"i18next/no-literal-string": ["error", { markupOnly: true }],
+		"max-len": ["error", { ignoreComents: true }]
 	},
 	globals: {
 		"__IS_DEV__": true
--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -4,8 +4,9 @@ export function classNames(cls: string, mods: Mods = {}, additional: string[] = 
 	return [
 		cls,
 		...additional.filter(Boolean),
-		Object.entries(mods)
-			.filter(([classNames, value]) => Boolean(value))
-			.map(([classNames, value]) => classNames)
-	].join(" ");
+		...Object.entries(mods)
+			.filter(([_, value]) => Boolean(value))
+			.map(([className]) => className),
+	]
+		.join(' ');
 }
