9b47ee28dd1ef82919129a013698fd07bb813a40
Onvix24 <maksdark2004@gmail.com>
2023-10-20 20:16:45 +0300

    add react testing library, add tests for btn sidebar

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,6 +1,6 @@
 import { classNames } from "shared/lib/classNames/classNames";
 import cls from "./Sidebar.module.scss";
-import React, { useState } from "react";
+import { useState } from "react";
 import { ThemeSwitcher } from "widgets/ThemeSwitcher";
 import { LangSwitcher } from "widgets/LangSwitcher";
 
